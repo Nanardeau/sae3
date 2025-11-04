27d8364d8abba4b670b04a89cb6e6e9af5624c50
Louennig <louennig.correc@etudiant.univ-rennes.fr>
2025-11-04 14:46:19 +0100

    Squelette Header

---
+++ html/index.php
@@ -0,0 +1,15 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alizon</title>
+</head>
+<body>
+    <header>
+        <img src="" alt="logo front">
+        <!--<input type="text" placeholder="Rechercher">-->
+        <input type="button" name="button" value="Se connecter">
+        <input type="button" name="button" value="Mon Panier">
+    </header>
+</body>
+</html>
