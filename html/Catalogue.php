4b3ba2a39da0e058409584a05f1d5a7fa766db7a
Johan Keraudren <jkeraudren@jkeraudren.local>
2025-11-04 18:34:36 +0100

    card 70% essaie plusieurs carte en catalogue

--- html/Catalogue.php
+++ html/Catalogue.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alizon</title>
-    <link href="./css/style/catalogue.css" rel="stylesheet">
+    <link href="./css/style/catalogue.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
@@ -17,13 +17,22 @@
             Catégorie <?php //Choisir la catégorie  
                         ?>
         </h2>
-        <div class="card">
-            <figcaption>
-                <img></img>
-                <caption>libelProduit</caption>
-            </figcaption>
-        </div>
-        
+        <article>
+        <?php 
+        for($i = 0; $i < 10;$i++){
+
+        ?>
+            <div class="card">
+                <figcaption>
+                    <img src="./img/img_test.jpg"></img>
+                    <caption>libelProduit</caption>
+                </figcaption>
+                <p class="prix">test</p>
+            </div>
+        <?php
+            }
+        ?>
+        <article>
 
 
     </main>
