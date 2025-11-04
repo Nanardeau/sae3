e2bff9f25c79e56dcf197a0cc55a8d6f53cf17e2
Johan Keraudren <jkeraudren@jkeraudren.local>
2025-11-04 14:43:53 +0100

    Ajout du debut des fichiers style

---
+++ html/Catalogue.php
@@ -0,0 +1,16 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alizon</title>
+    <link href="./css/style/catalogue.css" rel="stylesheet">
+</head>
+<body>
+    <header></header>
+    <main>
+        <h1>Toute les catégories<h1>
+            <h2>Catégorie <?php //Choisir la catégorie  ?></h2> 
+    </main>
+    <footer></footer>
+</body>
+</html>
