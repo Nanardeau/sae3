1fb786ecc99a54dd5f6c1004e8ad38e995b2724f
Johan Keraudren <jkeraudren@jkeraudren.local>
2025-11-05 22:11:38 +0100

    fichier de connexion avec la bdd

--- html/Catalogue.php
+++ html/Catalogue.php
@@ -1,3 +1,28 @@
+<?php 
+//Connexion à la base de données.
+require_once __DIR__ . '/_env.php';
+loadEnv(__DIR__ . '/.env');
+
+// Récupération des variables
+$host = getenv('PGHOST');
+$port = getenv('PGPORT');
+$dbname = getenv('PGDATABASE');
+$user = getenv('PGUSER');
+$password = getenv('PGPASSWORD');
+
+// Connexion à PostgreSQL
+try {
+    $ip = 'pgsql:host=' . $host . ';port=' . $port . ';dbname=' . $dbname . ';';
+    $bdd = new PDO($ip, $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+    echo "✅ Connecté à PostgreSQL ($dbname)";
+} catch (PDOException $e) {
+    echo "❌ Erreur de connexion : " . $e->getMessage();
+}
+
+?>
+
 <html lang="fr">
 
 <head>
