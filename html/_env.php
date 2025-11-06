e4c2d1795da1c68640d1c2aadde2a7044b047bdf
Louennig <louennig.correc@etudiant.univ-rennes.fr>
2025-11-06 10:16:02 +0100

    Ajout dossier includes footer et header

---
+++ html/_env.php
@@ -0,0 +1,11 @@
+<?php
+function loadEnv($path) {
+    if (!file_exists($path)) return;
+
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (str_starts_with(trim($line), '#')) continue; // Ignore les commentaires
+        [$name, $value] = explode('=', $line, 2);
+        putenv(trim($name) . '=' . trim($value));
+    }
+}
