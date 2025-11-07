2ce40fb63fa6d1e7f85e75736553455ae2fd7f13
etudiant <etudiant@pce-test>
2025-11-07 11:56:49 +0100

    php connexion

--- html/ConnexionClient.php
+++ html/ConnexionClient.php
@@ -1,3 +1,46 @@
+<?php
+//Connexion à la base de données.
+require_once __DIR__ . '/_env.php';
+loadEnv(__DIR__ . '/.env');
+
+// Récupération des variables
+$host = getenv('PGHOST');
+$port = getenv('PGPORT');
+$dbname = getenv('PGDATABASE');
+$user = getenv('PGUSER');
+$password = getenv('PGPASSWORD');
+
+
+// Connexion à PostgreSQL
+
+try {
+    $ip = 'pgsql:host=' . $host . ';port=' . $port . ';dbname=' . $dbname . ';';
+    $bdd = new PDO($ip, $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+    // "✅ Connecté à PostgreSQL ($dbname)";
+} catch (PDOException $e) {
+    // "❌ Erreur de connexion : " . $e->getMessage();
+}
+$bdd->query('set schema \'alizon\'');
+
+
+
+if($_POST){
+    $id= $_POST["pseudo"];
+    $mdp = $_POST["mdp"];
+    if($id=!"" && $mdp=!""){
+        $req= $bdd->query ("SELECT * FROM Client WHERE pseudo ='$pseudo' AND mdp= '$mdp'");
+        $rep= $req->fetch();
+        if($rep['id']=!false){
+            //connexion
+        }else{
+            $error_msg="Identifiant ou mot de passe incorrect.";
+        }
+    }
+}else{
+?>
+
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
@@ -9,12 +52,19 @@
 <body>
     <header><a href="accueil.php"><img src="../../img/logo_alizon_front.svg" alt="logo-alizon" title="logo-alizon"/></a></header>
     <main>
-        <form>
+        <form action="ConnexionClient.php" method="post">
             <h2>Connexion</h2>
             <label for="pseudo">Identifiant</label>
             <input type="text" name="pseudo" placeholder="Identifiant..." id="identifiant"/>
             <label for="mdp">Mot de passe</label>
             <input type="password" name="mdp" placeholder="Mot de passe..." id="mdpCli"/>
+            <?php 
+                if($error_msg){
+            ?>
+                    <p> <?php echo($error_msg); ?> </p>
+            <?php
+                }
+            ?>
             <input class="bouton" type="submit" value="Se connecter"/>
         </form>
         <aside>
@@ -33,3 +83,7 @@
     <footer></footer>
 </body>
 </html>
+<?php
+}
+?>
+
