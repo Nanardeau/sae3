de6353e6259eead8b1ee2a594fd99588c7124df5
luhan-gui <luhan.guiggia@medjed-dev.fr>
2025-11-07 14:03:36 +0100

    Correction des problèmes de Polices du header

--- html/backoffice/accueil.php
+++ html/backoffice/accueil.php
@@ -3,10 +3,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alizon BackOffice</title>
-    <link rel="stylesheet" href="../css/style/backOffice/backend.css" type="text/css">
+    <link rel="stylesheet" href="../css/style/backend.css" type="text/css">
 </head>
 <body>
-    
-    
+    <?php include '../includes/backoffice/header.php'; ?>
+    <main>
+        <h1>Bienvenue dans le BackOffice d'Alizon</h1>
+        <p>Utilisez le menu de navigation pour gérer les produits, les catégories et les commandes.</p>
+    </main>
 </body>
 </html>
