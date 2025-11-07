497ebcbba1276752808e215368f12690558ca36f
Luhan GUIGGIA--MILON <luhan.guiggia@medjed-dev.fr>
2025-11-07 10:57:03 +0100

    modif du fichier html en php

--- html/backoffice/accueil.php
+++ html/backoffice/accueil.php
@@ -2,8 +2,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alizon</title>
+    <title>Alizon BackOffice</title>
+    <link rel="stylesheet" href="../css/style/backOffice/backend.css" type="text/css">
 </head>
 <body>
+    
+    
 </body>
 </html>
