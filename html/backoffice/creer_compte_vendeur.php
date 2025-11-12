0c796d53a309b16d7b14d0721153eabe44f00272
Guimard <guimardtitouan40@gmail.com>
2025-11-12 10:42:20 +0100

    Ajouter des regex

--- html/backoffice/creer_compte_vendeur.php
+++ html/backoffice/creer_compte_vendeur.php
@@ -13,92 +13,99 @@
     </header>
     <main>
         <form action="creer_compte_vendeur.php" method="post">
-        <div>
-            
-            <h2>
-                Création de compte
-            </h2>
-            <h3>
-                Profil Responsable
-            </h3>
-            <h5>
-                Identifiant* :
-            </h5>
-            <input type="text" name="identifiant" id="identifiant" class="dimension" required>
-            <div class="nomPrenom">
-                    <div class="labelInput">
-                    <h5>
-                        Nom* :
-                    </h5>
-                    
-                    <input type="text" name="nom" id="nom"  class="dimension_petit" required>
+        <div class="alignementPR_IE">
+            <div>
+                
+                <h2>
+                    Création de compte
+                </h2>
+                <h3>
+                    Profil Responsable
+                </h3>
+                <h5>
+                    Identifiant* :
+                </h5>
+                <input type="text" name="identifiant" id="identifiant" class="dimension" required>
+                <div class="nomPrenom">
+                        <div class="labelInput">
+                            <h5>
+                                Nom* :
+                            </h5>
+                            
+                            <input type="text" name="nom" id="nom"  class="dimension_petit" required>
+                        </div>
+                        <div class="labelInput">
+                            <h5>
+                                Prenom* :
+                            </h5>
+                            <input type="text" name="prenom" id="prenom"  class="dimension_petit" required>
+                        </div>
                     </div>
-                    <div class="labelInput">
-                    <h5>
-                        Prenom* :
-                    </h5>
-                    <input type="text" name="prenom" id="prenom"  class="dimension_petit" required>
-                    </div>
-                </div>
-            
-            <h5>
-                Adresse e-mail* :
-            </h5>
-            <input type="text" name="email" id="email" class="dimension" required>
-            <h5>
-                Confirmer adresse e-mail* :
-            </h5>
-            <input type="text" name="email_conf" id="email_conf" class="dimension" required>
-            <h5>
-                Numéro de téléphone* :
-            </h5>
-            <input type="text" name="num_tel" id="num_tel" class="dimension_petit" required>
-            <h5>
-                mot de passe* :
-            </h5>
-            <input type="password" name="mdp" id="mdp" class="dimension" required>
-            <h5>
-                Confirmer mot de passe* :
-            </h5>
-            <input type="password" name="mdp_conf" id="mdp_conf" class="dimension" required>
+                
+                <h5>
+                    Adresse e-mail* :
+                </h5>
+                <input type="text" name="email" id="email" class="dimension" required>
+                <h5>
+                    Confirmer adresse e-mail* :
+                </h5>
+                <input type="text" name="email_conf" id="email_conf" class="dimension" required>
+                <h5>
+                    Numéro de téléphone* :
+                </h5>
+                <input type="text" name="num_tel" id="num_tel" class="dimension_petit" required>
+                <h5>
+                    mot de passe* :
+                </h5>
+                <input type="password" name="mdp" id="mdp" class="dimension" required>
+                <h5>
+                    Confirmer mot de passe* :
+                </h5>
+                <input type="password" name="mdp_conf" id="mdp_conf" class="dimension" required>
+            </div>
+            <hr>
+            <div>
+                <h3>
+                    Information entreprise
+                </h3>
+                <h5>
+                    Numéro de SIREN* :
+                </h5>
+                <input type="text" name="num_siren" id="num_siren" class="dimension" required>
+                <h5>
+                    Raison sociale* :
+                </h5>
+                <input type="text" name="raison_soc" id="raison_soc" class="dimension" required>
+                <h5>
+                    ligne d'adresse 1* :
+                </h5>
+                <input type="text" name="num_adresse1" id="num_adresse1" placeholder="Numéro de l'adresse" class="num_adresse" required>
+                <input type="text"name="rue_adresse1" id="rue_adresse1" placeholder="Rue" class="rue_adresse" required>
+                <h5>
+                    ligne d'adresse 2 :
+                </h5>
+                <input type="text" name="adresse2" id="adresse2" placeholder="Apt, suite, unité, nom de l’entreprise (facultatif)" class="dimension">
+                <h5>
+                    Code postal* :
+                </h5>
+                <input type="text" name="code_post" id="code_post" required>
+                <h5>
+                    Ville* :
+                </h5>
+                <input type="text" name="ville" id="ville" class="dimension" required>
+                
+                
+                
+            </div>
         </div>
-        <hr>
-        <div>
-            <h3>
-                Information entreprise
-            </h3>
-            <h5>
-                Numéro de SIREN* :
-            </h5>
-            <input type="text" name="num_siren" id="num_siren" class="dimension" required>
-            <h5>
-                Raison sociale* :
-            </h5>
-            <input type="text" name="raison_soc" id="raison_soc" class="dimension" required>
-            <h5>
-                ligne d'adresse 1* :
-            </h5>
-            <input type="text" name="num_adresse1" id="num_adresse1" placeholder="Numéro de l'adresse" class="num_adresse" required>
-            <input type="text"name="rue_adresse1" id="rue_adresse1" placeholder="Rue" class="rue_adresse" required>
-            <h5>
-                ligne d'adresse 2 :
-            </h5>
-            <input type="text" name="adresse2" id="adresse2" placeholder="Apt, suite, unité, nom de l’entreprise (facultatif)" class="dimension">
-            <h5>
-                Code postal* :
-            </h5>
-            <input type="text" name="code_post" id="code_post" required>
-            <h5>
-                Ville* :
-            </h5>
-            <input type="text" name="ville" id="ville" class="dimension" required>
-            <input type="submit">Créer le compte</input>
+        <div class="button">
+            <button type="submit" >Créer le compte</button>
         </div>
+        
+        
         </form>
     </main>
-    <footer>
-        
-    </footer>
+    
     <?php
         require_once('../env.php');
         
@@ -117,12 +124,13 @@
         try {
             $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;";
             $pdo = new PDO($dsn, $user, $password, [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-            ]);
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+        
             echo "✅ Connecté à PostgreSQL ($dbname)";
         } catch (PDOException $e) {
             echo "❌ Erreur de connexion : " . $e->getMessage();
-        }   
+        }
         
         //initialisation des variables
         $identifiant= $_POST["identifiant"];
@@ -140,33 +148,43 @@
         $adresse2= $_POST["adresse2"];
         $code_post= $_POST["code_post"];
         $ville= $_POST["ville"];
-
-        //insertion d'une adresse dans la base de données
-        $stmt = $pdo->prepare("INSERT INTO alizon.Adresse(num, nomRue, codePostal, nomVille) VALUES (:num, :nomRue, :codePostal, :nomVille)");
-        $stmt->execute(array(
-            ":num" => $num_adresse1,
-            ":nomRue" => $rue_adresse1,
-            ":codePostal" => $code_post,
-            ":nomVille" => $ville
-        ));
-
-        //Prise de l'id de l'adresse créée
-        $res = $pdo->query("SELECT idAdresse FROM alizon.Adresse ORDER BY idAdresse DESC LIMIT 1")->fetch();
-        $idAdresse = $res["idAdresse"];
-
-        //insertion d'un vendeur dans la base de données
-        $stmt = $pdo->prepare('INSERT INTO alizon.Vendeur(SIREN, email, identifiant, raisonSociale, idAdresseSiege, mdp) VALUES (:siren, :mail, :id, :raisonsoc, :idAdresseSiege, :mdp)');
-        $stmt->execute(array(
-            ":siren" => $num_siren,
-            ":id" => $identifiant,
-            ":raisonsoc" => $raison_soc,
-            ":idAdresseSiege" => $idAdresse, //insertion de l'id associé au vendeur
-            ":mail" => $email,
-            ":mdp" => $mdp
-        ));
-
         
+        if ($mdp==$mdp_conf && $email==$email_conf){
+            
+            //insertion d'une adresse dans la base de données
+            $stmt = $pdo->prepare("INSERT INTO alizon.Adresse(num, nomRue, codePostal, nomVille) VALUES (:num, :nomRue, :codePostal, :nomVille)");
+            $stmt->execute(array(
+                ":num" => $num_adresse1,
+                ":nomRue" => $rue_adresse1,
+                ":codePostal" => $code_post,
+                ":nomVille" => $ville,
+                ":complement" => $adresse2, 
+                ":numAppart" => $adresse2 //A revoir
+            ));
 
+            //Prise de l'id de l'adresse créée
+            $res = $pdo->query("SELECT idAdresse FROM alizon.Adresse ORDER BY idAdresse DESC LIMIT 1")->fetch();
+            $idAdresse = $res["idAdresse"];
+
+            //insertion d'un vendeur dans la base de données
+            $stmt = $pdo->prepare('INSERT INTO alizon.Vendeur(nom, prenom, numTel, SIREN, email, identifiant, raisonSociale, idAdresseSiege, mdp) VALUES (:nom, :prenom, :numtel, :siren, :mail, :id, :raisonsoc, :idAdresseSiege, :mdp)');
+            $stmt->execute(array(
+                ":nom" => $nom,
+                ":prenom" => $prenom,
+                ":numtel" => $num_tel,
+                ":siren" => $num_siren,
+                ":id" => $identifiant,
+                ":raisonsoc" => $raison_soc,
+                ":idAdresseSiege" => $idAdresse, //insertion de l'id associé au vendeur
+                ":mail" => $email,
+                ":mdp" => $mdp
+            ));
+        }
+        else {
+
+            echo "<p>le mot de passe ou l'email n'est pas le même dans la confirmation</p>";
+        }
+        
     ?>
 </body>
 </html>
