75d8c89655bd04e0c106481cb9409b461f92ff9c
Guimard <guimardtitouan40@gmail.com>
2025-11-13 10:13:11 +0100

    Creer la fiche produit vendeur

---
+++ html/backoffice/consulter_fiche_produit_vendeur.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>alizon</title>
+    </head>
+    <body>
+        
+        <?php include '../includes/backoffice/header.php';?>
+
+        
+        <main>
+            <img src="" alt="image du produit">
+            <div>
+                <h1>Libelle Produit</h1>
+                <div>
+                    <img src="" alt="etoile">
+                </div>
+                <input type="button" value="Afficher les commentaires">
+                <p>
+                    description produit..................................................................
+                    ...............
+                </p>
+                <hr>
+                <h2>00.00 €</h2>
+                <input type="button" value="Afficher les commentaires">
+                <input type="button" value="Afficher les commentaires">
+
+            </div>
+            <h2>Caractéristique</h2>
+            
+        </main>
+    </body>
+</html>
