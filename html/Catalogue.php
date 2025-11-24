f7602475ce199f5954b3d4f7062745cb8e51626d
Bernard MICHEL <bernardmicheel@gmail.com>
2025-11-24 20:31:56 +0100

    correction popup ajout panier

--- html/Catalogue.php
+++ html/Catalogue.php
@@ -49,6 +49,7 @@ $bdd->query('set schema \'alizon\'');
     ?>
 
     <main>
+        <?php if(isset($_GET["ajout"])):?>
         <div class="ajoutPanierFait">
             <div class="partieGauche" onclick="fermerPopUpPanier()">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check-icon lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
@@ -58,7 +59,7 @@ $bdd->query('set schema \'alizon\'');
                 <a href="Panier.php" class="bouton">Aller au panier</a>
             </div>
         </div>
-
+        <?php endif?>
         <h1>Toutes les catégories</h1>
 
 
@@ -148,7 +149,7 @@ $bdd->query('set schema \'alizon\'');
     <?php include 'includes/footer.php';?>
     <script>
         function fermerPopUpPanier(){
-            window.location.href = "index.php";
+            window.location.href = "Catalogue.php";
         }
     </script>
 </body>
