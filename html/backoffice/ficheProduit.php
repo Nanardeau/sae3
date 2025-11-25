d9d901f3f61c7dc0dad4afd7254baabf73cc53ce
Shalamin <Shalamin.pro@gmail.com>
2025-11-25 22:44:30 +0100

    modification des bouton retour, mise en place des fichiers css dans un dossier back

--- html/backoffice/ficheProduit.php
+++ html/backoffice/ficheProduit.php
@@ -33,8 +33,9 @@ if(!array_key_exists("codeCompte", $_SESSION) || !isset($_SESSION["codeCompte"])
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="../css/style/ficheProduit.css" >
-        <link rel="stylesheet" type="text/css" href="../css/style/popupAvis.css" >
+        <link rel="stylesheet" type="text/css" href="../css/style/backoffice/ficheProduit.css" >
+        <link rel="stylesheet" type="text/css" href="../css/style/backoffice/popupAvis.css" >
+        <link href="../css/components/fonts.css" rel="stylesheet" type="text/css">
         <script src="../js/FicheProd.js"></script>
         <title>alizon</title>
     </head>
@@ -44,12 +45,12 @@ if(!array_key_exists("codeCompte", $_SESSION) || !isset($_SESSION["codeCompte"])
 
         <main>
             <a href="index.php" class="btn-retour">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-chevron-left-icon lucide-square-chevron-left">
-            <rect width="18" height="18" x="3" y="3" rx="2"/>
-            <path d="m14 16-4-4 4-4"/>
-        </svg>
-        Retour
-    </a>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-chevron-left-icon lucide-square-chevron-left">
+                    <rect width="18" height="18" x="3" y="3" rx="2"/>
+                    <path d="m14 16-4-4 4-4"/>
+                </svg>
+                Retour
+            </a>
             <div class="alignemnt_droite_gauche">
                
                 <?php
