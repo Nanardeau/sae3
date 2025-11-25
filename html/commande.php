0516ffdc430c715c3884e3fdba09759ee42348cc
Shalamin <Shalamin.pro@gmail.com>
2025-11-26 00:03:34 +0100

    lien dans commande vers le detail produit

--- html/commande.php
+++ html/commande.php
@@ -137,7 +137,15 @@ $bdd->query('set schema \'alizon\'');
                     ?>
                     
                      <tr>
-                        <td><?php echo $nomProd['libelleprod'];?></td>
+                        <td>
+                            <a href="dproduit.php?id=<?= $idProd ?>" class="btn-prod">
+                            <?php echo $nomProd['libelleprod'];?>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right-icon lucide-arrow-up-right">
+                                    <path d="M7 7h10v10"/>
+                                    <path d="M7 17 17 7"/>
+                                </svg>
+                            </a>
+                        </td>
                         <td><?php echo $idProd ; ?></td>
                         <td><?php echo $prixTTC ;?></td>
                         <td><?php echo $qteprod ;?></td>
