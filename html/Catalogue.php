b0f6489d5d363c2127c6215f0c7a52e3a4cffc51
Shalamin <Shalamin.pro@gmail.com>
2026-01-16 14:53:14 +0100

    ajout des mentions légales et fichier connDB pour eviter la redondance

--- html/Catalogue.php
+++ html/Catalogue.php
@@ -1,30 +1,5 @@
 <?php
-session_start();
-
-
-//Connexion à la base de données.
-require_once( __DIR__ . '/_env.php');
-loadEnv(__DIR__ . '/.env');
-
-// Récupération des variables
-$host = getenv('PGHOST');
-$port = getenv('PGPORT');
-$dbname = getenv('PGDATABASE');
-$user = getenv('PGUSER');
-$password = getenv('PGPASSWORD');
-
-
-// Connexion à PostgreSQL
-
-try {
-    $ip = 'pgsql:host=' . $host . ';port=' . $port . ';dbname=' . $dbname . ';';
-    $bdd = new PDO($ip, $user, $password, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
-    // "✅ Connecté à PostgreSQL ($dbname)";
-} catch (PDOException $e) {
-    // "❌ Erreur de connexion : " . $e->getMessage();
-}
+include('connDb.php');
 $bdd->query('set schema \'alizon\'');
 
     $pmin = $_GET['pmin'] ??  null; 
