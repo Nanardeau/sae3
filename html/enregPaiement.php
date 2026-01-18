638f6d0c60c30021e6190f0f50b16f1d4dd3dfae
Bernard MICHEL <bernardmicheel@gmail.com>
2026-01-18 15:13:16 +0100

    Ajout confirmation paiement

--- html/enregPaiement.php
+++ html/enregPaiement.php
@@ -1,7 +1,7 @@
 <?php 
     session_start();
     require_once __DIR__ . '/_env.php';
-    header("location:confirmerPaiement.php");
+    header("location:paiementFini.php");
     loadEnv(__DIR__ . '/.env');
 
     $host = getenv('PGHOST');
